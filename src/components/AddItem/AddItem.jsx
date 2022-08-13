42aaac77ec2dd856d6ae9acffc1b60878de8aade
Justin Nicholas <nicholasj964@yahoo.com>
2022-08-13 18:52:46 -0500

    clear inputs upon submission of new picture and description

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -2,16 +2,28 @@ import {useState} from 'react'
 import './AddItem.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function AddItem({addPicture}) {
+function AddItem({addPicture, getPictures}) {
 
     const [url, setUrl] = useState('');
     const [des, setDes] = useState('');
 
+    const handleSubmit = (event) => {
+        const newUrl = url;
+        const newDes = des;
+        clearInputs();
+        addPicture(newUrl, newDes);
+    }
+
+    const clearInputs = () => {
+        setUrl('');
+        setDes('');
+    }
+
     return (
         <div className='inputBox' >
-            <input className='urlIn' type="text" placeholder="Image URL" onChange={(event) => setUrl(event.target.value)}/>
-            <input className='desIn' type="text" placeholder="Description" onChange={(event) => setDes(event.target.value)} />
-            <div className='addButton' onClick={() => addPicture(url, des)}><FontAwesomeIcon icon="fa-solid fa-circle-plus" /></div>
+            <input className='urlIn' type="text" placeholder="Image URL" onChange={(event) => setUrl(event.target.value)} value={url} />
+            <input className='desIn' type="text" placeholder="Description" onChange={(event) => setDes(event.target.value)} value={des} />
+            <div className='addButton' onClick={(event) => handleSubmit(event)}><FontAwesomeIcon icon="fa-solid fa-circle-plus" /></div>
         </div>
 
     )
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -6,7 +6,7 @@ function GalleryList({pictures, updateLikes, toggleImage, deletePicture}) {
     return(
         <>
         {pictures.map(picture => (
-            <div className='cardBody gy-5' key={picture.id}>
+            <div className='cardBody gy-2' key={picture.id}>
                 {/* will have to .map to go through all of the data in the array. */}
                 <GalleryItem pictures={pictures} id={picture.id} description={picture.description} path={picture.path} likes={picture.likes} showingimage={picture.showingimage} updateLikes={updateLikes} toggleImage={toggleImage} deletePicture={deletePicture} />
             </div>
